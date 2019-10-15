35d80d9157c69db62e3c8e5aab4851932116f48e
tenasatupitsyn <214782@bk.ru>
2019-10-14 22:13:17 -0300

    Fix pkg

--- session/index.js
+++ session/index.js
@@ -3,17 +3,22 @@
 const axios = require('axios');
 
 module.exports = async (username, password) => {
-	let csrfToken;
-	let sessionID;
+	let csrfToken, sessionID;
 
-	const { headers: getHeaders } = await axios.get('https://www.instagram.com/accounts/login/');
+	/**
+	 * Requesting csrftoken
+	 */
+	const { headers: _headers } = await axios.get('https://www.instagram.com/accounts/login/');
 
-	for (const k in getHeaders['set-cookie']) {
-		if (getHeaders['set-cookie'][k].match('^csrftoken=')) {
-			csrfToken = getHeaders['set-cookie'][k].split(';')[0].split('=')[1];
+	for (const k in _headers['set-cookie']) {
+		if (_headers['set-cookie'][k].match('^csrftoken=')) {
+			csrfToken = _headers['set-cookie'][k].split(';')[0].split('=')[1];
 		}
 	}
-
+	
+	/**
+	 * Generating new session ID
+	 */
 	const { headers, body } = await request({
 		method: 'POST',
 		url: 'https://www.instagram.com/accounts/login/ajax/',
@@ -39,7 +44,7 @@ module.exports = async (username, password) => {
 		}
 	} else {
 		return 'Username or password is incorrect. Please check and try again';
-	}	
+	}
 };
 
 function request(data) {
